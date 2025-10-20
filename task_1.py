8765fab7b3b9cfe45b3172f6e472448c0804e04c
Inna Saienko <inna.saienko.nl@gmail.com>
2025-10-20 11:12:51 +0200

    task1: switch returned values 0 and 1 to one statement

--- task_1.py
+++ task_1.py
@@ -4,12 +4,12 @@ def caching_fibonacci():
     def fibonacci(n):
         if n in cache:
             return cache[n]
-        if n<= 0:
-            return 0
-        if n == 1:
+
+        if n >= 1:
             return n
         else:
             result = fibonacci(n-1) + fibonacci(n-2)
+
         cache[n] = result
         return result
     return fibonacci
