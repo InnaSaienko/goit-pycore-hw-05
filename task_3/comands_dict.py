1adf8cc07f5167115610ddbff7d701e0c422378e
Inna Saienko <inna.saienko.nl@gmail.com>
2025-10-21 09:30:25 +0200

    task_3: create template for task

---
+++ task_3/comands_dict.py
@@ -0,0 +1,8 @@
+from typing import Dict, Callable
+
+operations: Dict[str, Callable] = {
+    'add': add,
+    'multiply': multiply,
+    'square': square,
+    'cube': cube
+}
---
+++ task_3/main.py
@@ -0,0 +1,16 @@
+import sys
+
+
+def main():
+    if len(sys.argv) < 2:
+        print("Використання: python main.py <шлях_до_файлу> [рівень_логування]")
+        sys.exit(1)
+
+    file_path = sys.argv[1]
+
+
+
+
+
+if __name__ == "__main__":
+    main()
