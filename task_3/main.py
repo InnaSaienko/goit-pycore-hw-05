af35773e0cf47952777399146b7813514fa9076c
Inna Saienko <inna.saienko.nl@gmail.com>
2025-10-21 11:32:39 +0200

    task_3: + read args from command line

--- task_3/main.py
+++ task_3/main.py
@@ -1,13 +1,17 @@
 import sys
 
+from comands_func import load_logs
+
 
 def main():
     if len(sys.argv) < 2:
-        print("Використання: python main.py <шлях_до_файлу> [рівень_логування]")
+        print(f"Used: python main.py {sys.argv}")
         sys.exit(1)
 
     file_path = sys.argv[1]
+    level_arg = sys.argv[2] if len(sys.argv) > 2 else None
 
+    logs = load_logs(file_path)
 
 
 
